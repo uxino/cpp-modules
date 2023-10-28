ebf3456113e2de72b77de950182e60f5c165106f
Muhammed Esad ŞEKER <76184668+uxino@users.noreply.github.com>
2023-10-28 18:19:27 +0300

    Add files via upload

--- cpp00/ex01/PhoneBook.cpp
+++ cpp00/ex01/PhoneBook.cpp
@@ -44,30 +44,48 @@ void PhoneBook::add(void)
 void PhoneBook::search(void)
 {
 	int	index;
-    cout << endl << "     index|     name|     surname|     nickname|";
-    cout << endl << "------------------------------------------------" << endl;
+    cout << endl << "     index|      name|  lastname|  nickname|";
+    cout << endl << "--------------------------------------------" << endl;
 	for (int i = 0; i < 4; i++)
 	{
-		cout << "         " << i << "|        " << this->contacts[i].get_name() << "|         "\
-			<< this->contacts[i].get_last_name() << "|              " << this->contacts[i].get_nickname() << "|" << endl;
+		cout << std::setw(10) << i << "|";
+		if (this->contacts[i].get_name().length() > 9)
+			cout << std::setw(10) << this->contacts[i].get_name().substr(0,9) + "." << "|";
+		else
+			cout << std::setw(10) << this->contacts[i].get_name() << "|";
+		if (this->contacts[i].get_last_name().length() > 9)
+			cout << std::setw(10) << this->contacts[i].get_last_name().substr(0,9) + "." << "|";
+		else
+			cout << std::setw(10) << this->contacts[i].get_last_name() << "|";
+		if (this->contacts[i].get_nickname().length() > 9)
+			cout << std::setw(10) << i << "|" << this->contacts[i].get_nickname().substr(0,9) + "." << "|" << endl;
+		else
+			std::cout << std::setw(10) << this->contacts[i].get_nickname() << "|" << endl;
 	}
 	while (1)
 	{
 		cout << "Please write index that you want to show." << endl;
-		std::cin >> index;
-		if (index >=0 && index <=8)
+		
+		if (std::cin >> index)
 		{
-			cout << "index|name|surname|nickname|phone_number|darkest_secret|" << endl;
-			cout << "    " << index << "|   " << this->contacts[index].get_name() << "|      "\
-			<< this->contacts[index].get_last_name() << "|       " << this->contacts[index].get_nickname() << "|           " \
-			<< this->contacts[index].get_phone_number() << "|             " << this->contacts[index].get_darkest_secret() << "|" << endl;
-			break;
+			std::cin.ignore(1,'\n');
+			if (index >=0 && index <=7)
+			{
+				cout << "index|name|surname|nickname|phone_number|darkest_secret|" << endl;
+				cout << "    " << index << "|   " << this->contacts[index].get_name() << "|      "\
+				<< this->contacts[index].get_last_name() << "|       " << this->contacts[index].get_nickname() << "|           " \
+				<< this->contacts[index].get_phone_number() << "|             " << this->contacts[index].get_darkest_secret() << "|" << endl;
+				break;
+			}
+			else
+				std::cout << "Invalid input. Please enter a valid index." << std::endl;
 		}
-		else
-		{
-			cout << "Please right input!! (0<8)" << endl;
-			continue;
-		}
+		else {
+            std::cout << "Invalid input. Please enter a valid index." << std::endl;
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+        }
 	}
 }
 
