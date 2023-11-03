1cf5650ae79a752f5794c0be2a5257d93c97e4a0
Muhammed Esad ŞEKER <76184668+uxino@users.noreply.github.com>
2023-11-03 23:44:23 +0300

    Add files via upload

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -5,12 +5,16 @@
 int main(int argc, char **argv)
 {
 	if (argc != 4)
+	{
 		std::cout << "Please give 4 argument!!" << std::endl;
+		return 0;
+	}
     std::string filename = argv[1];
     std::string oldWord = argv[2];
     std::string newWord = argv[3];
 
     std::ifstream inputFile(filename);
+    std::ofstream outputFile(filename + ".replace");
     std::string fileContents;
 
     if (inputFile.is_open())
@@ -19,14 +23,13 @@ int main(int argc, char **argv)
         while (std::getline(inputFile, line))
             fileContents += line + '\n';
         inputFile.close();
-
         size_t pos = 0;
         while ((pos = fileContents.find(oldWord, pos)) != std::string::npos)
 		{
-            fileContents.replace(pos, oldWord.length(), newWord);
+            fileContents.erase(pos, oldWord.length());
+			fileContents.insert(pos, newWord);
             pos += newWord.length();
         }
-        std::ofstream outputFile(filename);
         if (outputFile.is_open())
 		{
             outputFile << fileContents;
